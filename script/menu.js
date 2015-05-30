ab7f8715e3285eb859b52a3663377acc8699bebc
azinthehollow <yhcir_fh@outlook.com>
2015-05-29 21:14:50 -0600

    Metodo de menu solo aplica en test por ahora

---
+++ script/menu.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var menu = $('#menu');
+  var contenedor = $('#menu-contenedor');
+  var menu_offset = menu.offset();
+  // Cada vez que se haga scroll en la página se hara un chequeo del estado del menú y lo vamos a alternar entre 'fixed' y 'static'.
+  $(window).on('scroll', function() {
+    if($(window).scrollTop() > menu_offset.top) {
+      menu.addClass('menu-fijo');
+    } else {
+      menu.removeClass('menu-fijo');
+    }
+  });
+});
