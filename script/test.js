d4f53a5a2b3ba093ef87c8659c734f2e32d2aa94
azinthehollow <yhcir_fh@outlook.com>
2015-05-29 23:07:06 -0600

    Correccion se algunos errores ortograficos
    se agrego el metodo para hacer el test pero no esta completo

--- script/test.js
+++ script/test.js
@@ -7,10 +7,37 @@ $(document).ready(function(e) {
                var sum = 0;
 
                // Grupo 1
-               if ($("input:radio[name=RadioGroup1]").is(':checked')) {
-                  var n1 = $("input:radio[name=RadioGroup1]:checked").val();
+               if ($("input:radio[name=pregunta1]").is(':checked')) {
+                  var n1 = $("input:radio[name=pregunta1]:checked").val();
                   sum += parseFloat(n1);
                }
+               // Grupo 2
+               if ($("input:radio[name=pregunta2]").is(':checked')) {
+                  var n1 = $("input:radio[name=pregunta2]:checked").val();
+                  sum += parseFloat(n1);
+               }
+               // Grupo 3
+               if ($("input:radio[name=pregunta3]").is(':checked')) {
+                  var n1 = $("input:radio[name=pregunta3]:checked").val();
+                  sum += parseFloat(n1);
+               }
+               // Grupo 4
+               if ($("input:radio[name=pregunta4]").is(':checked')) {
+                  var n1 = $("input:radio[name=pregunta4]:checked").val();
+                  sum += parseFloat(n1);
+               }
+               // Grupo 5
+               if ($("input:radio[name=pregunta5]").is(':checked')) {
+                  var n1 = $("input:radio[name=pregunta5]:checked").val();
+                  sum += parseFloat(n1);
+               }
+               // Grupo 6
+               if ($("input:radio[name=pregunta6]").is(':checked')) {
+                  var n1 = $("input:radio[name=pregunta6]:checked").val();
+                  sum += parseFloat(n1);
+               }
+
+
 
                $('#resultado').val(sum);
 
