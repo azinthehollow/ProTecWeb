b1d8a951e977400c4ebe85efb4a004e7b4d977ee
azinthehollow <yhcir_fh@outlook.com>
2015-05-30 06:43:13 -0600

    Realse V1
    Falta incluir la api, y corregir algunos css de las letras para que se
    noten

--- script/menu.js
+++ script/menu.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var menu_offset = menu.offset();
   // Cada vez que se haga scroll en la página se hara un chequeo del estado del menú y lo vamos a alternar entre 'fixed' y 'static'.
   $(window).on('scroll', function() {
-    if($(window).scrollTop() > menu_offset.top) {
+    if ($(window).scrollTop() > menu_offset.top) {
       menu.addClass('menu-fijo');
     } else {
       menu.removeClass('menu-fijo');
